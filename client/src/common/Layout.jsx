ff7d8611dc52cc92444708480c84452e32e0a476
Thiago Nunes <thiagonunes.tns@gmail.com>
2024-01-31 23:18:09 -0300

    set active nav link

--- client/src/common/Layout.jsx
+++ client/src/common/Layout.jsx
@@ -1,33 +1,40 @@
 import React from "react";
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 function Layout({ children }) {
-  return (
-    <>
+
+    let [activeLink, setActiveLink] = React.useState('home');
+
+    const getClassName = (link) => {
+        return link === activeLink ? 'nav-link px-2 text-secondary' : 'nav-link px-2 text-white';
+    }
+
+    return (
+        <>
         <header className="p-3 bg-dark text-white">
             <div className="container">
-            <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-                <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
-                    <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="30" height="24" />
-                </a>
+                <div className="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                    <a href="/" className="d-flex align-items-center mb-2 mb-lg-0 text-white text-decoration-none">
+                        <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width="30" height="24" />
+                    </a>
 
-                <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-                <li><Link to="/" className="nav-link px-2 text-secondary">Home</Link></li>
-                <li><Link to="/menu" className="nav-link px-2 text-white">Menu</Link></li>
-                <li><a href="#" className="nav-link px-2 text-white">Orders</a></li>
-                <li><a href="#" className="nav-link px-2 text-white">Custumers</a></li>
-                <li><a href="#" className="nav-link px-2 text-white">Reports</a></li>
-                </ul>
+                    <ul className="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><NavLink to="/" onClick={() => setActiveLink('home')} className={() => getClassName('home')}>Home</NavLink></li>
+                    <li><NavLink to="/menu" onClick={() => setActiveLink('menu')} className={() => getClassName('menu')}>Menu</NavLink></li>
+                    <li><a href="#" className="nav-link px-2 text-white">Orders</a></li>
+                    <li><a href="#" className="nav-link px-2 text-white">Customers</a></li>
+                    <li><a href="#" className="nav-link px-2 text-white">Reports</a></li>
+                    </ul>
 
-                <div className="text-end">
-                    <button type="button" className="btn btn-outline-light me-2">Logout</button>
+                    <div className="text-end">
+                        <button type="button" className="btn btn-outline-light me-2">Logout</button>
+                    </div>
                 </div>
-            </div>
             </div>
         </header>
         {children}
-    </>
-  );
+        </>
+    );
 }
 
 export default Layout;
