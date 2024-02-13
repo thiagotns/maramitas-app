b67d804b753cd67ce275fea086d9c531bd894810
Thiago Nunes de Sousa <thiagonunes.tns@gmail.com>
2024-02-13 10:29:06 -0300

    create menu modal

--- client/src/menu/Menu.jsx
+++ client/src/menu/Menu.jsx
@@ -1,8 +1,12 @@
 import React, { useEffect } from "react";
 import { DataGrid } from '@mui/x-data-grid';
 import { Box, Button, Typography, Fab } from "@mui/material";
+import Modal from '@mui/material/Modal';
 import AddIcon from '@mui/icons-material/Add';
 import { useNavigate } from "react-router-dom";
+import { DatePicker } from '@mui/x-date-pickers';
+import Stack from '@mui/material/Stack';
+import dayjs from 'dayjs';
 
 const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
@@ -10,10 +14,31 @@ const columns = [
     { field: 'end_date', headerName: 'End name', width: 130 }
   ];
 
+const modalStyle = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
 function Menu() {
+
+    let newMenu = {
+        start_date: new Date(),
+        end_date: new Date()
+    };
 
     const navigate = useNavigate();
     const [menuList, setMenuList] = React.useState([]);
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
     useEffect(() => {
         fetch('/api/menu')
@@ -30,8 +55,16 @@ function Menu() {
         navigate(`/menu/${row.id}`);
     }
 
-    const handleCreateButtonClick = (row) => {
-        navigate('/menu/create');
+    const handleCreateButtonClick = () => {
+        newMenu = {
+            start_date: new Date(),
+            end_date: new Date()
+        }
+        handleOpen();
+    }
+
+    const handleSaveButtonClick = () => {
+        console.log(newMenu);
     }
 
     return (
@@ -52,6 +85,26 @@ function Menu() {
                 Create
             </Button>
         </Box>
+        <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+        >
+            <Box sx={modalStyle}>
+                <Typography id="modal-modal-title" variant="h6" component="h2" sx={{marginBottom: 2}}>
+                    Create Menu
+                </Typography>
+                <Stack spacing={2}>
+                    <DatePicker label="Start Date" value={dayjs(newMenu.start_date)}/>
+                    <DatePicker label="End Date" value={dayjs(newMenu.end_date)}/>
+                    <Box sx={{display: "flex", justifyContent: "flex-end"}}>
+                        <Button color="primary" onClick={handleClose} sx={{marginRight: 1}}>Cancel</Button>
+                        <Button variant="contained" color="primary" onClick={handleSaveButtonClick}>Save</Button>
+                    </Box>
+                </Stack>
+            </Box>
+        </Modal>
         <DataGrid
             rows={menuList}
             columns={columns}
