91761b1a7d6dc3ff84b32c2129b378c0a6985305
Thiago Nunes de Sousa <thiagonunes.tns@gmail.com>
2024-02-14 17:42:44 -0300

    Refactor serializers and views for MenuItem

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -6,16 +6,23 @@ class OptionSerializer(serializers.ModelSerializer):
         model = Option
         fields = ('id', 'name', 'price')
 
-class MenuItemSerializer(serializers.ModelSerializer):
+class MenuItemReadSerializer(serializers.ModelSerializer):
     options = OptionSerializer(many=True)
 
     class Meta:
         model = MenuItem
+        fields = ('id', 'name', 'description', 'type', 'menu', 'options', 'options_name')
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MenuItem
         fields = ('id', 'name', 'description', 'type', 'menu', 'options')
 
+
 class MenuSerializer(serializers.ModelSerializer):
-    items = MenuItemSerializer(many=True)
+    items = MenuItemReadSerializer(many=True, read_only=True)
+    avaliable_options = OptionSerializer(many=True, read_only=True)
 
     class Meta:
         model = Menu
-        fields = ('id', 'start_date', 'end_date', 'items')
+        fields = ('id', 'start_date', 'end_date', 'items', 'avaliable_options')
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Menu, Option, MenuItem
-from .serializers import MenuSerializer, OptionSerializer, MenuItemSerializer
+from .serializers import MenuSerializer, OptionSerializer, MenuItemSerializer, MenuItemReadSerializer
 
 class MenuViewSet(viewsets.ModelViewSet):
     queryset = Menu.objects.all()
@@ -13,4 +13,15 @@ class OptionViewSet(viewsets.ModelViewSet):
 
 class MenuItemViewSet(viewsets.ModelViewSet):
     queryset = MenuItem.objects.all()
-    serializer_class = MenuItemSerializer
+    
+    def get_serializer_class(self):
+         # Define your HTTP method-to-serializer mapping freely.
+         # This also works with CoreAPI and Swagger documentation,
+         # which produces clean and readable API documentation,
+         # so I have chosen to believe this is the way the
+         # Django REST Framework author intended things to work:
+         if self.request.method in ['GET']:
+             # Since the ReadSerializer does nested lookups
+             # in multiple tables, only use it when necessary
+             return MenuItemReadSerializer
+         return MenuItemSerializer
