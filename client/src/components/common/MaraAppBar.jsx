95668d0488da91042ac97b827ed5c23648299f87
Thiago Nunes de Sousa <thiagonunes.tns@gmail.com>
2024-02-19 17:44:04 -0300

    remove invalid image

--- client/src/components/common/MaraAppBar.jsx
+++ client/src/components/common/MaraAppBar.jsx
@@ -150,7 +150,7 @@ function MaraAppBar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                {/*<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />*/}
               </IconButton>
             </Tooltip>
             <Menu
