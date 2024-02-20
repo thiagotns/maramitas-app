b0f397e9eaf3ff7cded8bd13c081cfe2bf4a8a44
Thiago Nunes de Sousa <thiagonunes.tns@gmail.com>
2024-02-20 05:31:40 -0300

    save jwt into localhistorage

--- client/src/contexts/AppAuthProvider.js
+++ client/src/contexts/AppAuthProvider.js
@@ -4,7 +4,12 @@ const AppAuthContext = createContext([]);
 
 export const AppAuthProvider = ({ children }) => {
 
-    const [appAuth, setAppAuth] = useState({});
+    const [appAuth, setAppAuth_] = useState(JSON.parse(localStorage.getItem('appAuth')) || {});
+
+    const setAppAuth = (appAuth) => {
+        localStorage.setItem('appAuth', JSON.stringify(appAuth));
+        setAppAuth_(appAuth);
+    }
 
     return (
         <AppAuthContext.Provider value={[appAuth, setAppAuth]}>
